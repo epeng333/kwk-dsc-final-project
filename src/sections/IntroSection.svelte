eae5d08f272a76496ccfcd242194dd39da815b28
epeng333 <bethpeng86@yahoo.com>
2025-07-19 13:27:50 -0700

    Add IntroSection and graphic 1 financial-well-being

---
+++ src/sections/IntroSection.svelte
@@ -0,0 +1,111 @@
+<div class="container">
+  <h1>Introduction</h1>
+  <p>
+    When I opened <i>Rich Dad, Poor Dad</i> by Robert T. Kiyosaki this past June,
+    I did not expect the book to pique my curiosity about the world of personal finance—a
+    world where knowing how to manage and increase wealth can lead to achieving financial
+    security and freedom, and a world that I was intimidated by because I was not
+    very knowledgeable about it.
+  </p>
+  <!-- <p>Hello, my name is <strong>{name}</strong>!</p> -->
+  <!-- <p>My favorite number is <strong>{favoriteNumber}</strong>.</p> -->
+  <p>
+    Who knew I would encounter terms like <i>assets</i>, <i>debts</i>,
+    <i>businesses</i>, and <i>homeownership</i> again after reading
+    <i>Rich Dad, Poor Dad</i> when I joined the Kode with Klossy and Black Wealth
+    Data Center’s Data Storytelling Challenge one week later?
+  </p>
+
+  <p>
+    Based on data on the
+    <a href="https://blackwealthdata.org/">Black Wealth Data Center website</a>,
+    it quickly became clear that the Black population is often at a disadvantage
+    when looking at various wealth indicators —factors such as employment and
+    homeownership that can be used to measure financial well-being—and seeing
+    the striking disparities between Black and other racial groups in the United
+    States.
+  </p>
+
+  <p>
+    Compared to White and Asian populations in the U.S., Black people have the
+    <strong>lowest total assets</strong>, the <strong>lowest net worth</strong>,
+    and the <strong>highest percent of unbanked households</strong>. In several
+    states and counties, Black people have <strong>fewer employer firms</strong>
+    and a <strong>lower median income</strong> than other races.
+  </p>
+
+  <p>
+    Additionally, according to an
+    <a
+      href="https://blackwealthdata.org/resources/library/How-Racial-Disparities-in-Small-Business-Ownership-Affect-Black-Wealth-and-Economic-Prosperity"
+      >Urban Institute report</a
+    >, Black entrepreneurs own only <strong>2.5% of employer firms</strong>,
+    while White business owners own 74.8% of employer firms.
+  </p>
+
+  <p>
+    Even though business equity is the second largest type of household wealth
+    (with homeownership being the first), Black families only hold <strong
+      >1.4% of the United States’ $30.8 trillion in business equity</strong
+    >, with Black families having a median business equity of $42,000, which is
+    much less than White families’ median business equity of $105,000.
+  </p>
+
+  <p>
+    And yet, while I was scrolling through all of these disparities on Black
+    Wealth Data Center’s website, a certain
+    <a href="https://blackwealthdata.org/explore/assets">graphic</a> stood out to
+    me.
+  </p>
+
+  <img
+    class="financial-well-being-img"
+    src="financial-well-being.png"
+    alt="Financial Well-Being"
+  />
+
+  <p>
+    In 2023, Black survey respondents had the <strong>highest percentage</strong
+    >
+    of respondents who said they were <strong>better off (29.2%)</strong> out of
+    all of the racial groups (Asian, Hispanic, and White).
+  </p>
+
+  <p>
+    Additionally, Black respondents had the <strong
+      >highest increase (4.5%)</strong
+    >
+    in those who said they were <strong>better off</strong> in 2023 than they
+    were in 2020, as well as the <strong>largest decrease (-3.6%)</strong> in
+    those who said they were <strong>worse off</strong> in 2023 than they were in
+    2020.
+  </p>
+
+  <p>
+    How can that be when there are so many wealth indicators that show the
+    significant financial disparities between Black people and people of other
+    races?
+  </p>
+
+  <p>This led me to the following question:</p>
+
+  <p>
+    <strong
+      >What factors could have caused an increase in Black respondents who say
+      that they are better off in 2023 than in 2020?</strong
+    >
+  </p>
+</div>
+
+<style>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 20px;
+    /* text-align: center; */
+  }
+  .financial-well-being-img {
+    width: 100%;
+    margin: 0px auto;
+  }
+</style>
